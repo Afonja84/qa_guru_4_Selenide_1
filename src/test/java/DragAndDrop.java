e9fd66fdf8284a26157f25a5e532ba4681825e53
Evgeny Usatenko <usatenko_e@mail.ru>
2021-11-21 13:51:49 +0300

    Добавил sleep для наглядности

--- src/test/java/DragAndDrop.java
+++ src/test/java/DragAndDrop.java
@@ -3,8 +3,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class DragAndDrop {
     @BeforeAll
@@ -15,8 +14,10 @@ public class DragAndDrop {
     @Test
     void drugAndDropTest(){
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        sleep(3000); //увидеть что было
         $("#column-a").dragAndDropTo("#column-b");
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
+        sleep(3000); //увидеть что стало
     }
 }
